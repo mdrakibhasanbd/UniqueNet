fb476c8c06a53f7c96c6c468863414d9e228c4a6
mdrakibhasanbd <mdrakibhasanbd369@gmail.com>
2023-10-12 05:40:30 +0600

    remove Unused File

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // component: HomeView
-      // component: () => import('../views/Navbar1.vue')
-      // component: () => import('../views/LandingPage2.vue')
-      component: () => import('../views/LandingPage1.vue')
-      // component: () => import('../views/Slider.vue')
-      // component: () => import('../views/Carousel.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LandingPage.vue')
     }
   ]
 })
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,18 +1,13 @@
 <template>
-    <!-- <Navbar /> -->
 
 <Banner />
 
 </template>
 <script>
-import Navbar from '../components/Navbar.vue';
 import Banner from '../components/Banner.vue';
-import Slider from '../components/Slider.vue';
 export default {
     components: {
-        Navbar,
         Banner,
-        Slider,
     },
 };
 
